2ec4c6a5831b9b6b2c098cae6fc9bcac4e062769
MaksimPeterburgskiy <maksim.peter777@gmail.com>
2023-10-27 21:34:24 +0000

    inital home page commit of blank page

--- old/views.py
+++ old/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import LaundryMachine, Kasa, KasaPowerReading, DiscordHook
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
+
+
+def index(request):
+    context1 = {
+        "LaundryMachines": LaundryMachine.objects.all(),
+        "Kasas": Kasa.objects.all(),
+        "DiscordHooks": DiscordHook.objects.all(),
+    }
+    return render(request,"index.html",context=context1)
+
+def data(request):
+    context = {
+        "LaundryMachines": LaundryMachine.objects.all(),
+        "Kasas": Kasa.objects.all(),
+    }
+    return JsonResponse(model_to_dict(context))
