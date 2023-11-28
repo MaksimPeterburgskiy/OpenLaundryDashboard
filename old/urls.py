3c20e10a518fa1317ee7a2100785ed0e502902c4
MaksimPeterburgskiy <maksim.peter777@gmail.com>
2023-11-28 21:54:23 +0000

    Added Avail button to template and created view to proccess request.

--- old/urls.py
+++ old/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('machine_data', views.machine_data, name='machine_data'),
+    path('set_available/<int:pk>/', views.set_available, name='set_available'),
 ]
--- old/views.py
+++ old/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import LaundryMachine, Kasa, KasaPowerReading, DiscordHook
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
@@ -20,3 +20,13 @@ def machine_data(request):
     machines = LaundryMachine.objects.all()
     data = serializers.serialize("json", machines)
     return HttpResponse(data, content_type='application/json')
+
+def set_available(request, pk):
+    # get machine by pk
+    machine = LaundryMachine.objects.get(pk=pk)
+    # set machine to available
+    machine.status = "A"
+    # save machine
+    machine.save()
+    # return success
+    return redirect(index)
